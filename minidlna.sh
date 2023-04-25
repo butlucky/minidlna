5105e334d57799644dfd5731ba4c6d987eacff1d
hewenwen <hewenwen.523@bytedance.com>
2023-04-25 14:37:23 +0800

    fix flac issue, when aarch64 compile optimize minidlna.sh script
    Signed-off-by: hewenwen <hewenwen.523@bytedance.com>

--- minidlna.sh
+++ minidlna.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+CROSS_VAL   = "/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf-"
+GCC_VAL     = "/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc"
+STRIP_VAL   = "/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf-strip"
+INCLUDE_VAL = "/home/hewenwen/minidlna/minidlna-deps/include"
+LIBS_VAL    = "/home/hewenwen/minidlna/minidlna-deps/lib"
+HOST_VAL    = "aarch64-linux-gnu"
+PRE_VAL     = "/home/hewenwen/minidlna/minidlna-deps"
+OUTPUT_VAL  = "/home/hewenwen/minidlna/output"
+
 if [ $# == 0 ];then
 echo -e "\n---------------prepare file first----------------\n"
 cd backup
 tar vzxf ffmpeg-6.0.tar.gz -C ../
-tar vxf flac-1.4.2.tar.xz -C ../
+tar vxf  flac-1.3.4.tar.xz -C ../
 tar vjxf jpeg-6b.tar.bz2 -C ../
 tar vjxf libexif-0.6.24.tar.bz2 -C ../
 tar vzxf libid3tag_0.15.1b.orig.tar.gz -C ../
@@ -22,63 +31,63 @@ fi
 
 cd libexif-0.6.24
 echo -e "\n\n---------------start to compile libexif----------------\n\n"
-CC=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CPPFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib ./configure --host=arm-linux-gnu --prefix=/home/hewenwen/minidlna/minidlna-deps --disable-shared
+CC=$GCC_VAL CFLAGS=-I$INCLUDE_VAL CPPFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL ./configure --host=$HOST_VAL --prefix=$PRE_VAL --disable-shared
 make -j16; make install
 cd ..
 
 cd jpeg-6b 
 echo -e "\n\n---------------start to compile jpeg----------------\n\n"
-CC=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CPPFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib ./configure --host=arm-linux-gnu --prefix=/home/hewenwen/minidlna/minidlna-deps --disable-shared
+CC=$GCC_VAL CFLAGS=-I$INCLUDE_VAL CPPFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL ./configure --host=$HOST_VAL --prefix=$PRE_VAL --disable-shared
 make -j16; make install; make install-lib ; make install-headers
 cd ..
 
 cd zlib-1.2.11 
 echo -e "\n\n---------------start to compile zlib----------------\n\n"
-CROSS_PREFIX=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CPPFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib ./configure --prefix=/home/hewenwen/minidlna/minidlna-deps --static
+CROSS_PREFIX=$CROSS_VAL CFLAGS=-I$INCLUDE_VAL CPPFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL ./configure --prefix=$PRE_VAL --static
 make -j16; make install
 cd ..
 
 cd libid3tag-0.15.1b
 echo -e "\n\n---------------start to compile libid3tag----------------\n\n"
-CC=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CPPFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib ./configure --host=arm-linux-gnu --prefix=/home/hewenwen/minidlna/minidlna-deps --disable-shared
+CC=$GCC_VAL CFLAGS=-I$INCLUDE_VAL CPPFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL ./configure --host=$HOST_VAL --prefix=$PRE_VAL --disable-shared
 make -j16; make install
 cd ..
 
 cd libogg-1.3.5 
 echo -e "\n\n---------------start to compile libogg----------------\n\n"
-CC=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CPPFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib ./configure --host=arm-linux-gnu --prefix=/home/hewenwen/minidlna/minidlna-deps --disable-shared
+CC=$GCC_VAL CFLAGS=-I$INCLUDE_VAL CPPFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL ./configure --host=$HOST_VAL --prefix=$PRE_VAL --disable-shared
 make -j16; make install
 cd ..
 
-cd flac-1.4.2 
+cd flac-1.3.4 
 echo -e "\n\n---------------start to compile flac----------------\n\n"
-CC=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CXX=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++ CXXFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib ./configure --host=arm-linux-gnu --prefix=/home/hewenwen/minidlna/minidlna-deps --disable-shared --with-ogg=/home/hewenwen/minidlna/minidlna-deps
+CC=$GCC_VAL CFLAGS=-I$INCLUDE_VAL CXX=$CROSS_VALg++ CXXFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL ./configure --host=$HOST_VAL --prefix=$PRE_VAL --disable-shared --with-ogg=$PRE_VAL
 make -j16; make install
 cd ..
 
 cd libvorbis-1.3.7 
 echo -e "\n\n---------------start to compile vorbis----------------\n\n"
-CC=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CPPFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib ./configure --host=arm-linux-gnu --prefix=/home/hewenwen/minidlna/minidlna-deps --disable-shared --with-ogg=/home/hewenwen/minidlna/minidlna-deps
+CC=$GCC_VAL CFLAGS=-I$INCLUDE_VAL CPPFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL ./configure --host=$HOST_VAL --prefix=$PRE_VAL --disable-shared --with-ogg=$PRE_VAL
 make -j16; make install
 cd ..
 
 cd sqlite-autoconf-3410200
 echo -e "\n\n---------------start to compile sqlite----------------\n\n"
-CC=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CPPFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib ./configure --host=arm-linux-gnu --prefix=/home/hewenwen/minidlna/minidlna-deps --disable-shared
+CC=$GCC_VAL CFLAGS=-I$INCLUDE_VAL CPPFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL ./configure --host=$HOST_VAL --prefix=$PRE_VAL --disable-shared
 make -j16; make install
 cd ..
 
 cd ffmpeg-6.0
 echo -e "\n\n---------------start to compile ffmpeg----------------\n\n"
-./configure --prefix=/home/hewenwen/minidlna/minidlna-deps --cross-prefix=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf- --pkg-config=/usr/bin/pkg-config --arch=arm --target-os=linux --disable-shared --extra-cflags=-I/home/hewenwen/minidlna/minidlna-deps/include  --extra-ldflags=-L/home/hewenwen/minidlna/minidlna-deps/lib
+./configure --prefix=$PRE_VAL --cross-prefix=$CROSS_VAL --pkg-config=/usr/bin/pkg-config --arch=arm --target-os=linux --disable-shared --extra-cflags=-I$INCLUDE_VAL  --extra-ldflags=-L$LIBS_VAL
 make -j16; make install
 cd ..
 
 # need automake 1.16.1
 cd minidlna-1.3.2
 echo -e "\n\n---------------start to compile minidlna----------------\n\n"
-STRIP=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-strip CC=/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc CFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include CPPFLAGS=-I/home/hewenwen/minidlna/minidlna-deps/include LDFLAGS=-L/home/hewenwen/minidlna/minidlna-deps/lib LIBS="-lpthread -lexif -ljpeg -lsqlite3 -lavformat -lavcodec -lswresample -lavutil -lz -lid3tag -lFLAC -logg -lvorbis -ldl -lm" ./configure --host=arm-linux-gnu --prefix=/home/hewenwen/minidlna/output
+STRIP=$STRIP_VAL CC=$GCC_VAL CFLAGS=-I$INCLUDE_VAL CPPFLAGS=-I$INCLUDE_VAL LDFLAGS=-L$LIBS_VAL LIBS="-lpthread -lexif -ljpeg -lsqlite3 -lavformat -lavcodec -lswresample -lavutil -lz -lid3tag -lFLAC -logg -lvorbis -ldl -lm" ./configure --host=$HOST_VAL --prefix=$OUTPUT_VAL
 make -j16; make install
 cd ..
-/home/hewenwen/rv1126/prebuilts/gcc/linux-x86/arm/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-strip output/sbin/minidlnad
+$STRIP_VAL output/sbin/minidlnad
 
